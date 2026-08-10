23bc347896a9b348961518cf0e6241cfa676eca9
Gabriel Litvin <gabealitvin@gmail.com>
2026-08-09 21:30:09 -0400

    question

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('modal__overlay--visible');
+  const success = document.querySelector('modal__overlay--visible');
+  loading.class += " modal__overlay-visible"
+  emailjs
+  .sendForm(
+    'service_32mv14y',
+  'template_wsaudea',
+  event.target,
+'tZJL41HLK_cGHMi58'
+).then(()=> { loading.classList.remove("modal__overlay--visible");
+  success.classList += "modal__overlay--visible";
+}).catch (() => {
+  loading.classList.remove("modal__overlay--visible");
+  alert(
+    "The email service is temporarily unavailable. Please contact me directly on gabealitvin@gmail.com"
+  );
+})
+}
